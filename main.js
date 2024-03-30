cbba1b42899dd82f9c496652a6958e4514392049
ADILAHAMEDK <adilahamedk5842@gmail.com>
2024-03-30 22:11:23 +0530

    logic

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
- // Qestion is aa = 10; bb = 20; cc = 30; swibe the value
-      // 30,  10, 20
-      let aa = 10;
-      let bb = 20;
-      let cc = 30;
-      let total1 = aa + bb;
-      let total2 = bb - aa;
-      let total3 = cc - bb + 10;
-      aa = total1;
-      bb = total2;
-      cc = total3;
+ //(Q1) find the sum of two numbers and show the sum in webpage
+ let a = 100;
+ let b = 100;
+ let total = a + b;
+ let q1Browser = document.getElementById('q1').innerHTML = "sum is" + total;
 
-      console.log(aa, bb, cc);
+ //(Q2) aa = 10; bb = 20; cc = 30; swibe the value
+ // 30,  10, 20
+ let aa = 10;
+ let bb = 20;
+ let cc = 30;
+ let total1 = aa + bb;
+ let total2 = bb - aa;
+ let total3 = cc - bb + 10;
+ aa = total1;
+ bb = total2;
+ cc = total3;
 
-//(Q1) find the Largest number and Smallest number 
+console.log(aa, bb, cc);
+
+//(Q3) find the Largest number and Smallest number 
 let num1 = 50;
 let num2 = 30;
 
@@ -22,7 +28,7 @@ if(num1 > num2 ){
     console.log("Smallest number is" + num2);
 }
 
-//(Q2) find the value is a even number
+//(Q4) find the value is a even number
 let number1 = 50;
 
 if(number1 % 2==0){
@@ -30,3 +36,4 @@ if(number1 % 2==0){
 }else{
     console.log("this is a odd number");
 }
+
